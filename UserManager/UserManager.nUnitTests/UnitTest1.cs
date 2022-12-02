db93582422da9e2862ced8b413c9098a97a2b68c
JacobGuld <jgje18@student.aau.dk>
2022-12-02 12:13:05 +0100

    Added test

---
+++ UserManager/UserManager.nUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UserManager.nUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- UserManager/UsermanagerService/Models/DBService.cs
+++ UserManager/UsermanagerService/Models/DBService.cs
@@ -4,27 +4,27 @@ namespace UsermanagerService.Models
 {
     public class DBService
     {
-        private string _URL = "";
-
-        public DBService()
-        {
-            
-
-        }
+        private string URL = "";
 
         public bool ContainsCredentials(string username, string password)
         {
-
+            return false;
         }
 
-        public User FetchUser (int userId)
+        public User? FetchUser (int userId)
         {
-
+            return null;
+            //return new User(1, "test", "Aalborg", "AAU", "Student");
         }
 
         public bool DeleteUser(int userId)
         {
+            return false;
+        }
 
+        public bool AddNewUser(User user)
+        {
+            return false;
         }
        
 
