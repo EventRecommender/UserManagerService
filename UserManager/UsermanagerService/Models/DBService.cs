842d604dadbfd74e685b38f1b300bc21a9c55dc5
Villiam <Vjacob19@student.aau.dk>
2022-12-13 12:39:13 +0100

    Connection per call
    the service failed upon multiple request using one shared connection.

--- UserManager/UsermanagerService/Models/DBService.cs
+++ UserManager/UsermanagerService/Models/DBService.cs
@@ -15,12 +15,10 @@ namespace UsermanagerService.Models
 {
     public class DBService
     {
-        private MySqlConnection connection;
-
+        private string connectionString = "";
         public DBService(string uRL)
         {
-            connection = new MySqlConnection(uRL);
-            
+            connectionString = uRL;
         }
         
         /// <summary>
@@ -33,6 +31,8 @@ namespace UsermanagerService.Models
         /// <exception cref="DatabaseException"></exception>
         public void FetchUserAndPasswordFromUsername(string username, out User user, out string password)
         {
+            var connection = new MySqlConnection(connectionString);
+
             List<Tuple<User, string>> FoundUsers = new List<Tuple<User, string>>();
 
             //Query
@@ -42,10 +42,10 @@ namespace UsermanagerService.Models
                            $"WHERE username = '{username}';";
             
             //Command
-            MySqlCommand command = new MySqlCommand(query, this.connection);
+            MySqlCommand command = new MySqlCommand(query, connection);
             try
             {
-                this.connection.Open();
+                connection.Open();
                 MySqlDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
@@ -57,7 +57,7 @@ namespace UsermanagerService.Models
                 }
 
                 reader.Close();
-
+                connection.Close();
                 if (FoundUsers.Count == 1)
                 {
                     user = FoundUsers[0].Item1;
@@ -66,9 +66,9 @@ namespace UsermanagerService.Models
                 }
                 else { throw new InstanceException($"{FoundUsers.Count}"); }
             }
-            catch (MySqlException) { throw new DatabaseException("Database Error"); }
-            finally { if (this.connection.State == ConnectionState.Open) { this.connection.Close(); } }
-
+            catch (MySqlException) {
+                connection.Close();
+                throw new DatabaseException("Database Error"); }
         }
         
        /// <summary>
@@ -80,16 +80,18 @@ namespace UsermanagerService.Models
        /// <exception cref="DatabaseException"></exception>
         public User FetchUserFromID (int userId)
         {
+            var connection = new MySqlConnection(connectionString);
+
             List<User> users = new List<User>();
             //Query 
             string query = $"SELECT * FROM user WHERE id = {userId}";
 
             //SQL Command
-            MySqlCommand command = new MySqlCommand(query, this.connection);
+            MySqlCommand command = new MySqlCommand(query, connection);
 
             try
             {
-                this.connection.Open();
+                connection.Open();
                 MySqlDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
@@ -99,7 +101,8 @@ namespace UsermanagerService.Models
 
                 reader.Close();
                 command.Dispose();
-               
+                connection.Close();
+
                 if (users.Count == 1)
                 {
                     return users[0]; 
@@ -107,8 +110,9 @@ namespace UsermanagerService.Models
                 else { throw new InstanceException($"{users.Count}"); }
 
             }
-            catch (MySqlException ex) { throw new DatabaseException(ex.Message); }
-            finally { if (this.connection.State == ConnectionState.Open) { this.connection.Close(); } }
+            catch (MySqlException ex) {
+                connection.Close();
+                throw new DatabaseException(ex.Message); }
         }
 
         /// <summary>
@@ -119,27 +123,28 @@ namespace UsermanagerService.Models
         /// <exception cref="DatabaseException"></exception>
         public bool DeleteUser(int userId)
         {
+            var connection = new MySqlConnection(connectionString);
 
             //Queries (password is deleted in the cascade)
             string query1 = $"DELETE FROM user WHERE id = '{userId}';";
 
             //Command
-            MySqlCommand delUsercommand = new MySqlCommand(query1, this.connection);
+            MySqlCommand delUsercommand = new MySqlCommand(query1, connection);
             try
             {
-                this.connection.Open();
+                connection.Open();
                 int affected = delUsercommand.ExecuteNonQuery(); //delete the user from user table
+                connection.Close();
                 if (affected == 0)
                     return false;
            
                 return true;
             }
-            catch (MySqlException){
-
+            catch (MySqlException)
+            {
+                connection.Close();
                 throw new DatabaseException("DATABASE ERROR");
             }
-            finally { if (this.connection.State == ConnectionState.Open) { this.connection.Close(); } }
-
         }
 
         /// <summary>
@@ -151,6 +156,8 @@ namespace UsermanagerService.Models
         /// <exception cref="DatabaseException"></exception>
         public int AddUser(UIntUser user)
         {
+            var connection = new MySqlConnection(connectionString);
+
             int id = -1;
             //Queries
 
@@ -166,13 +173,13 @@ namespace UsermanagerService.Models
 
            
             //Sql Commands
-            MySqlCommand insertCommand = new MySqlCommand(insertQuery, this.connection); //The command to insert user into user table
-            MySqlCommand IdCommand = new MySqlCommand(getIdQuery, this.connection); //The command to retrieve the id of the inserted user.W
+            MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection); //The command to insert user into user table
+            MySqlCommand IdCommand = new MySqlCommand(getIdQuery, connection); //The command to retrieve the id of the inserted user.W
             
             
             try
             {
-                this.connection.Open();
+                connection.Open();
                 var tr = connection.BeginTransaction();
                 insertCommand.Transaction = tr;
                 IdCommand.Transaction = tr;
@@ -189,24 +196,27 @@ namespace UsermanagerService.Models
                 IdCommand.Dispose();
                 insertCommand.Dispose();
                 // Execute insertion in password table
-                MySqlCommand passwordCommand = new MySqlCommand($"INSERT INTO password VALUES ({id},'{user.Password}')", this.connection);
+                MySqlCommand passwordCommand = new MySqlCommand($"INSERT INTO password VALUES ({id},'{user.Password}')", connection);
                 passwordCommand.ExecuteNonQuery();
                 passwordCommand.Dispose();
 
                 tr.Commit();
+                connection.Close();
                 return id;
             }
-            catch (InsertionException) { return -1; }
-            catch (MySqlException)
-            {
-                throw new DatabaseException("DATABASE ERROR");
-            }
-            finally
-            {
-                if (this.connection.State == ConnectionState.Open) { this.connection.Close(); }
+            catch (InsertionException) 
+            { 
                 IdCommand.Dispose();
                 insertCommand.Dispose();
-                             
+                connection.Close();
+                return -1; 
+            }
+            catch (MySqlException)
+            {
+                IdCommand.Dispose();
+                insertCommand.Dispose();
+                connection.Close();
+                throw new DatabaseException("DATABASE ERROR");
             }
         }
         /// <summary>
@@ -216,26 +226,27 @@ namespace UsermanagerService.Models
         /// <exception cref="DatabaseException"></exception>
         public List<User> FetchAllUsers()
         {
+            var connection = new MySqlConnection(connectionString);
+
             List<User> users = new List<User>();
                     
             //Command
-            MySqlCommand Fetch = new MySqlCommand("SELECT * FROM user", this.connection);
+            MySqlCommand Fetch = new MySqlCommand("SELECT * FROM user", connection);
             try
             {
-                this.connection.Open();
+                connection.Open();
                 MySqlDataReader reader = Fetch.ExecuteReader();
 
                 while (reader.Read()) { 
                     users.Add(new User((int)reader[0], (string)reader[1], (string)reader[2], (string)reader[3], (string)reader[4]));
                 }
 
+                connection.Close();
                 return users;
             }catch(MySqlException ex)
             {
+                connection.Close();
                 throw new DatabaseException(ex.Message);
-            }
-            finally {
-                if (this.connection.State == ConnectionState.Open) { this.connection.Close(); }
             }
         }
     }
